fd6b68f824b3814e40e24315e73c77b4443410b3
mohendrathapa418@gmail.com <mohendrathapa418@gmail.com>
2023-12-19 21:51:26 +0530

    syllabus

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded',
+  function() {
+    const coll = document.querySelectorAll('.collapsible');
+    coll.forEach(function(item) {
+      item.addEventListener('click', function() {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+          content.style.display = 'none';
+        } else {
+          content.style.display = 'block';
+        }
+      });
+    });
+  });
+
